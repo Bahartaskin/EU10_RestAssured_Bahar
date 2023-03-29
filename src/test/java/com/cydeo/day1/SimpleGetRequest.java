c7353b3581dcff0dfdb0cf578d357d534f8de56e
Bahar Taskin <cakirbhr7@gmail.com>
2023-03-29 21:14:21 +0300

    Initial commit

---
+++ src/test/java/com/cydeo/day1/SimpleGetRequest.java
@@ -0,0 +1,26 @@
+package com.cydeo.day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+
+    String url = "http://52.204.187.175:8000/api/spartans";
+
+
+    @Test
+    public void test1(){
+
+
+        Response response = RestAssured.get(url);
+
+        System.out.println(response.statusCode());
+
+        response.prettyPrint();
+
+
+    }
+
+}
